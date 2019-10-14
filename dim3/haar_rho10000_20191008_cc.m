c2f53958f3ac1c61f34cb048ee714fadb8557008
dongli2017 <32219026+dongli2017@users.noreply.github.com>
2019-10-14 09:14:33 +0800

    Add files via upload

---
+++ dim3/haar_rho10000_20191008_cc.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%
+%%% Coh & Purity
+%%% 2019/10/08
+%%%%%%%%%%%%%%%%%%%%
+
+addpath(genpath('D:\\Software\\QElab\\QETLAB-0.9'))
+dim = 3;
+num =10^4;
+
+cc = [];
+purity = [];
+
+load('E:\\06_Coh\\00_Code\\00_dim3gs\\00_data\\a0_20190524_rho3_10000.mat','rho')
+
+for i = 1:num
+    rhotmp = rho(:,:,i);
+    cctmp = CoherenceWeight(rhotmp);
+    pptmp = Purity(rhotmp);
+    cc = cat(1, cc, cctmp);
+    purity = cat(1, purity, pptmp);
+    i
+end
+save('E:\\06_Coh\\00_Code\\00_dim3gs\\00_data\\a0_20191008_cc_10000.mat','cc')
+save('E:\\06_Coh\\00_Code\\00_dim3gs\\00_data\\a0_20191008_purity_10000.mat','purity')
